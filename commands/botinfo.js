e10072d360f52d95ed6f984636027a924bccc9d4
TehPigYT <34095153+TehPigYT@users.noreply.github.com>
2019-08-27 16:09:37 +0300

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {//no need to type all the 4 of them, just the needed ones
-
-    let embed = new Discord.RichEmbed()//We're going to use embeds to make the text look better and more understandable
+module.exports.run = async (bot, message, args) => {
+  
+  let embed = new Discord.RichEmbed()//We're going to use embeds to make the text look better and more understandable
     .setAuthor(message.author.username, message.author.avatarURL)//Getting and providing information about the author of the message
     .setDescription("Here's some information about this bot!")//Setting the description of the embed
     .setColor("RANDOM")//This is my choice, but embeds support hex colors, too! If you want to input a certain color, here's what you have to need That's it!
@@ -22,4 +22,4 @@ module.exports.run = async (bot, message, args) => {//no need to type all the 4 
 
 module.exports.help = {
   name: "botinfo"
-}
+}   
